6720ed590235ee22b9db1f7523016dc5f5f9503e
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-06 10:48:25 -0600

    have some styling done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import PlayerList from './components/PlayerList';
+import styled from 'styled-components';
+import Logo from './images/World-Cup-Logo.png';
 
 export default class App extends Component {
 
@@ -31,24 +33,65 @@ export default class App extends Component {
 
   render(){
     return(
-      <div className="App">
-        {this.state.playerData.map(player =>{
-          return <PlayerList
-            name={player.name}
-            country={player.country}
-            searches={player.searches}
-            key={player.id}
-          />
-        })}
-      </div>
+      <StyledApp>
+        <div className="App">
+          <div className="title">
+            <h1>Women's World Cup Players</h1>
+            <h2>Ranked by Search Interest June-July 2019</h2>
+          </div>
+          <div className="middleContent">
+            <img src={Logo} alt="Women's World Cup Logo 2019"/>
+            <div>
+              {this.state.playerData.map(player =>{
+                return <PlayerList
+                  name={player.name}
+                  country={player.country}
+                  searches={player.searches}
+                  key={player.id}
+                />
+              })}
+            </div>
+          </div>
+        </div>
+      </StyledApp>
     )
   }
-
-
-
-
-
-
-
-
 }
+
+const StyledApp = styled.div `
+  .App{
+
+    .title{
+      background-color: #edf2ee
+      opacity: .8;
+      padding: 3%;
+    }
+    
+    h1{
+      font-size: 3rem;
+      text-align: center;
+    }
+
+    h2{
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    .middleContent{
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: center;
+
+      img{
+        height: 100%;
+        padding: 5%;
+        margin: 1%;
+        background-color: #edf2ee;
+        opacity: .8;
+        border-radius: 10px;
+        box-shadow: 5px 5px 5px 0:
+      }
+
+    }
+  }
+`;
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import { List } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 const PlayerList = (props) =>{
   return(
-    <div>
-      <List className="listContainer">
-        <List.Item>
-          <List.Content>
-            <List.Header>{props.name}</List.Header>
-            <List.Description>From: {props.country}</List.Description>
-            <List.Description>Number of Searches: {props.searches}</List.Description>
-          </List.Content>
-        </List.Item>
-      </List>
-    </div>
+    <StyledPlayerList>
+      <div>
+        <List className="listContainer">
+          <List.Item>
+            <List.Content>
+              <List.Header className="playerName">{props.name}</List.Header>
+              <List.Description className="playerCountry">Country: {props.country}</List.Description>
+              <List.Description className="playerSearches">Number of Searches: {props.searches}</List.Description>
+            </List.Content>
+          </List.Item>
+        </List>
+      </div>
+    </StyledPlayerList>
   )
 }
+
+const StyledPlayerList = styled.div `
+  .listContainer{
+    width: 45%;
+    background-color: #edf2ee;
+    opacity: .8;
+    border-radius: 10px;
+    padding: 3%;
+    text-align: center;
+    margin: 5%;
+
+
+    .playerName{
+      font-size: 2.5rem;
+      font-weight: bold;
+    }
+
+    .playerCountry{
+      font-size: 1.2rem;
+      padding: 5%;
+    }
+
+    .playerSearches{
+      font-size: 1rem;
+    }
+  }
+`;
 
 export default PlayerList;
