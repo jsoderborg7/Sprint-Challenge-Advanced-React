46317511d14f2de028a61c5cf1106c0bcf0f9da5
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-06 12:03:42 -0600

    added a couple tests, they work

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test('App renders without crashing', () => {
+  render(<App />);
 });
+
+test('contains player Rose Lavelle', async() =>{
+  const myDoc = render(<App />)
+  myDoc.findByText(/Rose Lavelle/i);
+})
