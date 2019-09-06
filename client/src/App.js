92db69b22d81ebae8d505e275f4fbf65928225b4
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-06 09:38:37 -0600

    player information showing up on page!

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import PlayerList from './components/PlayerList';
 
 export default class App extends Component {
 
@@ -9,6 +10,38 @@ export default class App extends Component {
     this.state = {
       playerData: []
     }
+  }
+
+  componentDidMount(){
+    this.getPlayer();
+  }
+
+  getPlayer = () =>{
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res =>{
+        console.log(res.data);
+        const playerData = res.data;
+        this.setState({playerData})
+      })
+      .catch(err =>{
+        console.log(err);
+      })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        {this.state.playerData.map(player =>{
+          return <PlayerList
+            name={player.name}
+            country={player.country}
+            searches={player.searches}
+            key={player.id}
+          />
+        })}
+      </div>
+    )
   }
 
 
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,2 +1,20 @@
 import React from 'react';
-import { List, Image } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
+
+const PlayerList = (props) =>{
+  return(
+    <div>
+      <List className="listContainer">
+        <List.Item>
+          <List.Content>
+            <List.Header>{props.name}</List.Header>
+            <List.Description>From: {props.country}</List.Description>
+            <List.Description>Number of Searches: {props.searches}</List.Description>
+          </List.Content>
+        </List.Item>
+      </List>
+    </div>
+  )
+}
+
+export default PlayerList;
